d6dccc38a00e5d054442ce410993115041a239a2
Ananya Gupta <anaya29003@gmail.com>
2024-07-10 02:12:26 +0530

    Setting Up Navnbar

---
+++ frontend/src/Components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../../Utils/Navbar";
+
+const Home = () => {
+  return (
+    <>
+      <div className="h-screen h-min-screen w-full bg-[#FAFBFC]">
+        <Navbar />
+      </div>
+    </>
+  );
+};
+
+export default Home;
---
+++ frontend/src/Utils/Navbar.jsx
@@ -0,0 +1,60 @@
+import { profileIcon, shoppingBagIcon, wishlistIcon } from "./Icons";
+const Navbar = () => {
+  return (
+    <>
+      <div className="w-full h-[6vmax]">
+        <div
+          className="flex justify-center gap-[3vmax] items-center w-full h-full bg-[#FFFFFF] text-[
+#282C3F] font-font1 text-[14px] font-bold"
+        >
+          <img src="../assets/images/myntralogo.png" alt="not available" className="h-[50%]" />
+          <div className="flex gap-[3vmax]">
+            <div>
+              <p>MEN</p>
+            </div>
+            <div>
+              <p>WOMEN</p>
+            </div>
+            <div>
+              <p>KIDS</p>
+            </div>
+            <div>
+              <p>HOME & LIVING</p>
+            </div>
+            <div>
+              <p>BEAUTY</p>
+            </div>
+            <div>
+              <p>STUDIO</p>
+            </div>
+          </div>
+
+          <div>
+            <input
+              type="text"
+              placeholder="Search for products, brands and more"
+              className="rounded-sm border-[1px] focus:outline-none font-normal w-[30vmax] px-[1vmax] py-[0.5vmax] bg-[#F5F5F6]"
+            />
+          </div>
+
+          <div className="flex gap-[2vmax]">
+            <div className="flex flex-col justify-center items-center">
+              {profileIcon}
+              <h1>Profile</h1>
+            </div>
+            <div className="flex flex-col justify-center items-center cursor-pointer hover:scale-[0.9] transition-all hover:transition-all ">
+              {wishlistIcon}
+              <h1>Wishlist</h1>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+              {shoppingBagIcon}
+              <h1>Bag</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
